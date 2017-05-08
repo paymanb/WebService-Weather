d1cfd2b81e13d1d55fa036420e8f7432424c9499
Payman Bayat <payman1983@gmail.com>
2017-05-08 09:10:45 +0430

    Initial Commit

---
+++ WebService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebService
+//
+//  Created by Payman Bayat on 2/18/1396 AP.
+//  Copyright © 1396 AP Payman Bayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
